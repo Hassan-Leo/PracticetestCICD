fddbb6fa53a40dba2bf864f18da0444036d8e02d
Hassan-Leo <HassanLeo@github.com>
2021-05-29 15:52:28 +0500

    again udpdated indextest.js

--- test/indextest.js
+++ test/indextest.js
@@ -14,10 +14,20 @@ describe("Testing the api response data which is received",()=> {
     }) */
     it("To check response status",()=> {
         chai.request(server)
-        .get('')
+        .get("")
         .end((err, resp)=>{
             expect(resp).to.have.status(200);
         })
     })
-
+    it("To check data set of reponse",()=> {
+        chai.request(server)
+        .get("")
+        .end((err, resp)=> {
+            for(i=0;i<resp.body.length;i++){
+                expect(resp.body[i]).to.have.property("nam1");
+                expect(resp.body[i]).to.have.property("RollNo");
+                expect(resp.body[i]).to.have.property("student");
+            }
+        })
+    })
 })
